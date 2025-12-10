152720d2adaeb80f5f6ded860dca174bb5cd2a74
thiago25.mss <thiago25.mss@gmail.com>
2025-12-10 13:46:09 +0000

    fix: Change webhook URL to avoid Next.js conflict

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -1,21 +1,17 @@
 
 import express from 'express';
-import cors from 'cors';
-import metaRoutes from './routes/meta.js'; // Importando nossa futura rota
+import metaRoutes from './routes/meta.js';
 
 const app = express();
 
-// Middlewares essenciais
-app.use(cors()); // Permite requisições de outros domínios
-app.use(express.json()); // Habilita o parsing de JSON no corpo das requisições
+app.use(express.json());
 
-// Rota principal da API
-app.use('/api/meta', metaRoutes);
-
-// Rota de "saúde" para verificar se o serviço está no ar
 app.get('/', (req, res) => {
-  res.send('Servidor do WhatsApp está rodando!');
+  res.send('Servidor do Whatsapp está rodando!');
 });
+
+// Alterando a rota para evitar conflitos com o Next.js
+app.use('/whatsapp-api', metaRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
