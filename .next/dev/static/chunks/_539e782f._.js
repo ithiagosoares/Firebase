c88147e455eaeae82549afda4ec8c01a4006e980
thiago25.mss <thiago25.mss@gmail.com>
2025-12-23 11:13:40 +0000

    fix(auth): Corrige link de Esqueceu sua senha?

--- .next/dev/static/chunks/_539e782f._.js
+++ .next/dev/static/chunks/_539e782f._.js
@@ -575,7 +575,7 @@ function LoginPage() {
                                                 columnNumber: 15
                                             }, this),
                                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                                                href: "#",
+                                                href: "/forgot-password",
                                                 className: "ml-auto inline-block text-sm underline",
                                                 children: "Esqueceu sua senha?"
                                             }, void 0, false, {
